0a9af9eb3749fe068a9af081515881f0a19d1d27
skanderkaroui <skander.karoui@gmail.com>
2024-10-02 22:01:35 +0100

    project icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,12 +18,25 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "Skander's Porfolio",
   description: "Skander Karoui's minimalistic porfolio",
+  icons: {
+    icon: [
+      {
+        url: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🥶</text></svg>",
+        type: 'image/svg+xml',
+      },
+    ],
+  },
 };
-
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable}`}>
+      <head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🎤</text></svg>"
+        />
+      </head>
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
