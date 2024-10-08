8bdbb6f266cb38a82bf6817219ccf67adf1c8a06
skanderkaroui <skander.karoui@gmail.com>
2024-10-08 17:39:16 +0100

    new update

--- app/timeline/page.tsx
+++ app/timeline/page.tsx
@@ -21,33 +21,48 @@ export default function TimelinePage() {
             <div className="border-l-2 border-primary pl-4 pb-1">
               <h2 className="text-xl font-semibold mb-2">September 2024</h2>
               <ul className="list-disc list-inside space-y-2">
-                <li className="text-muted-foreground">Completed AI Software Engineering Fellowship at Headstarter.</li>
-                <li className="text-muted-foreground">Built AI Customer Assistant using Next.js, Gemini API, and RAG.</li>
-                <li className="text-muted-foreground">Developed AI Flashcard App with Next.js and Firebase.</li>
+                <li className="text-muted-foreground">
+                  Took part in the <Link href="https://www.linkedin.com/posts/skander-karoui_happy-to-share-that-ive-recently-took-part-activity-7247153360540192769-t4BV?utm_source=share&utm_medium=member_desktop" className="text-grey-500 italic hover:underline">2024 IPOC Challenge</Link> where I got to represent my school in Ghana.
+                </li>
               </ul>
             </div>
 
             <div className="border-l-2 border-primary pl-4 pb-1">
               <h2 className="text-xl font-semibold mb-2">August 2024</h2>
               <ul className="list-disc list-inside space-y-2">
-                <li className="text-muted-foreground">Worked on the Pantry App Tracker using Next.js, React, and Firebase.</li>
-                <li className="text-muted-foreground">Deployed Pantry App Tracker on Vercel.</li>
-                <li className="text-muted-foreground">Led Orbeats Club Radio ENET'com with 30+ active members.</li>
+                <li className="text-muted-foreground">Developed an <Link href="https://github.com/skanderkaroui/AI-Flashcard-App" className="text-grey-500 italic hover:underline">AI Flashcard App</Link> with Next.js and Firebase as part of Headstarter AI program.</li>
+                <li className="text-muted-foreground">Built an <Link href="https://github.com/skanderkaroui/customer-support-ai" className="text-grey-500 italic hover:underline">AI Customer Assistant</Link> using Next.js, Gemini API, and RAG as part of Headstarter AI program.</li>
               </ul>
             </div>
 
             <div className="border-l-2 border-primary pl-4 pb-1">
               <h2 className="text-xl font-semibold mb-2">July 2024</h2>
               <ul className="list-disc list-inside space-y-2">
-                <li className="text-muted-foreground">Interned at Belive Technology as an AI intern where I built an AI voice assistant.</li>
+                <li className="text-muted-foreground">Worked on a <Link href="https://pantryapp-pi.vercel.app/" className="text-grey-500 italic hover:underline">Pantry App Tracker</Link> using Next.js, React, and Firebase as part of Headstarter AI program.</li>
               </ul>
             </div>
 
             <div className="border-l-2 border-primary pl-4 pb-1">
               <h2 className="text-xl font-semibold mb-2">June 2024</h2>
               <ul className="list-disc list-inside space-y-2">
+                <li className="text-muted-foreground">Started working on <Link href="https://github.com/skanderkaroui/auto" className="text-grey-500 italic hover:underline">auto</Link> an AI voice assistant built using Faster Whisper, OpenAI and Google Text to Speech.</li>
                 <li className="text-muted-foreground">Participated in Climate Change AI’s virtual program.</li>
-                <li className="text-muted-foreground">Received 3 NVIDIA certifications:
+                <li className="text-muted-foreground">Received 3 Nvidia AI certifications:
+                  <ul className="list-disc list-inside space-y-1 mt-1 pl-5">
+                    <li><Link href="https://learn.nvidia.com/certificates?id=Pj1043ItSWKnf_--S-LMPQ" className="text-grey-500 italic hover:underline">Fundamentals of Deep Learning</Link></li>
+                    <li><Link href="https://learn.nvidia.com/certificates?id=ecpfbX4ETgeQhNl1n0PuyA" className="text-grey-500 italic hover:underline">Applications of AI for Predictive Maintenance</Link></li>
+                    <li><Link href="https://learn.nvidia.com/certificates?id=7LNTaxf0QKGtbsnGZ_-vEQ" className="text-grey-500 italic hover:underline">Building Transformer-Based NLP Applications</Link></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+
+            <div className="border-l-2 border-primary pl-4 pb-1">
+              <h2 className="text-xl font-semibold mb-2">May 2024</h2>
+              <ul className="list-disc list-inside space-y-2">
+                <li className="text-muted-foreground">Worked on a <Link href="https://github.com/skanderkaroui/auto" className="text-grey-500 italic hover:underline">Movie Recommendation System</Link> alongside my friend <Link href="https://www.linkedin.com/in/oussama-boubaker-18aa15241?miniProfileUrn=urn%3Ali%3Afs_miniProfile%3AACoAADwg-7oBnMSRCuguLrNrJdWBZONxtvBwKU4&lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_all%3BMy%2Fg2UCgSP6B4sZj3dGGPA%3D%3D" className="text-red-500 italic hover:underline">Bouba</Link> where we explored User-Based, Item-Based and Content-Based Collaborative Filtering.</li>
+                <li className="text-muted-foreground">Participated in Climate Change AI’s virtual program.</li>
+                <li className="text-muted-foreground">Received 3 Nvidia AI certifications:
                   <ul className="list-disc list-inside space-y-1 mt-1 pl-5">
                     <li><Link href="https://learn.nvidia.com/certificates?id=Pj1043ItSWKnf_--S-LMPQ" className="text-grey-500 italic hover:underline">Fundamentals of Deep Learning</Link></li>
                     <li><Link href="https://learn.nvidia.com/certificates?id=ecpfbX4ETgeQhNl1n0PuyA" className="text-grey-500 italic hover:underline">Applications of AI for Predictive Maintenance</Link></li>
@@ -60,6 +75,7 @@ export default function TimelinePage() {
             <div className="border-l-2 border-primary pl-4 pb-1">
               <h2 className="text-xl font-semibold mb-2">2023</h2>
               <ul className="list-disc list-inside space-y-2">
+                <li className="text-muted-foreground">I was selected as a Team Leader for Orbeats Radio Club ENET'Com where I served from June 2023 to June 2024.</li>
                 <li className="text-muted-foreground">Completed Omdena Kenya Chapter project on shoplifting detection.</li>
                 <li className="text-muted-foreground">Developed a Streamlit app using yolov8 and PytorchVideo Library.</li>
                 <li className="text-muted-foreground">Interned at ELYADATA as a Data Engineer, using Docker and FastAPI.</li>
@@ -73,8 +89,6 @@ export default function TimelinePage() {
                     <li>Data Scientist with Python Certification</li>
                   </ul>
                 </li>
-                <li className="text-muted-foreground">Led projects and gained experience in React Native, Agile methodologies, and project management at ENET'Com Junior Enterprise.</li>
-                <li className="text-muted-foreground">Designed web and mobile prototypes using Figma.</li>
               </ul>
             </div>
 
