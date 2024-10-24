fe8c5952b4f87258735fcb960a2c7a02449a1239
skanderkaroui <skander.karoui@gmail.com>
2024-10-24 09:51:33 +0100

    small corrections

--- components/portfolio.tsx
+++ components/portfolio.tsx
@@ -9,6 +9,8 @@ import {
   ChevronRight,
   Moon,
   Sun,
+  Instagram,
+  Facebook,
 } from "lucide-react";
 import Link from "next/link";
 import { useState, useEffect, useCallback } from "react";
@@ -24,14 +26,40 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-
-import projectsData from "@/app/data/projects.json";
-import RecommendationImg from "@/app/data/images/recommendation.jpg";
-import DefaultImg from "@/app/data/images/cat.jpeg";
 import SkanderImg from "@/app/data/images/ghana_photo_hozirontal.jpg";
 
-const projectImages = {
-  "1": RecommendationImg,
+// import projectsData from "@/app/data/projects.json";
+// import RecommendationImg from "@/app/data/images/recommendation.jpg";
+// import DefaultImg from "@/app/data/images/cat.jpeg";
+
+// const projectImages = {
+//   "1": RecommendationImg,
+// };
+const LeetCodeStats = () => {
+  const [solvedCount, setSolvedCount] = useState("around 150");
+
+  useEffect(() => {
+    const fetchLeetCodeStats = async () => {
+      try {
+        const response = await fetch(
+          "https://leetcodeapi-v1.vercel.app/skirrrrrra"
+        );
+        const data = await response.json();
+        const totalSolved =
+          data.skirrrrrra.submitStatsGlobal.acSubmissionNum.find(
+            (item: { difficulty: string; count: number }) =>
+              item.difficulty === "All"
+          ).count;
+        setSolvedCount(totalSolved);
+      } catch (error) {
+        console.error("Failed to fetch LeetCode stats:", error);
+      }
+    };
+
+    fetchLeetCodeStats();
+  }, []);
+
+  return <span>{solvedCount}</span>;
 };
 
 export function PortfolioComponent() {
@@ -82,7 +110,7 @@ export function PortfolioComponent() {
             height={300}
             style={{
               objectFit: "cover",
-              marginTop:"100px",
+              marginTop: "100px",
               marginLeft: "auto",
               marginRight: "auto",
               marginBottom: "30px",
@@ -149,100 +177,107 @@ export function PortfolioComponent() {
             <div className="text-center">
               <p className="text-primary">
                 I am a final-year engineering student from Tunisia pursuing a
-                degree in ICT engineering. I am highly involved in my local
-                community and have a strong passion for advancing AI and Data
-                Science, with a particular interest in software engineering and
-                AI.
+                degree in Telecommunication engineering. I am mostly interested
+                in AI and entrepreneurship and would love to contribute to any
+                project that can spark my interest.
               </p>
             </div>
           </section>
 
           <section id="projects">
-            <h2 className="text-2xl font-semibold mb-6">Recent Projects</h2>
-            <div className="relative">
-              <div className="overflow-hidden" ref={emblaRef}>
-                <div className="flex">
-                  {projectsData.map((project, index) => (
-                    <div key={index} className="flex-[0_0_100%] min-w-0 pl-4">
-                      <Card>
-                        <CardHeader>
-                          <CardTitle>{project.title}</CardTitle>
-                          <CardDescription>
-                            {project.description}
-                          </CardDescription>
-                        </CardHeader>
-                        <CardContent>
-                          <div className="relative w-full h-48 mb-4">
-                            <Image
-                              src={
-                                projectImages[
-                                  project.id.toString() as keyof typeof projectImages
-                                ] || DefaultImg
-                              }
-                              alt={project.title}
-                              fill
-                              style={{ objectFit: "cover" }}
-                              className="rounded-md"
-                            />
-                          </div>
-                        </CardContent>
-                        <CardContent>
-                          <p className="text-muted-foreground">
-                            {project.content}
-                          </p>
-                        </CardContent>
-                        <CardFooter>
-                          <Link
-                            href={project.link}
-                            className="text-primary hover:underline inline-flex items-center"
-                          >
-                            View Project <ArrowRight className="ml-2 h-4 w-4" />
-                          </Link>
-                        </CardFooter>
-                      </Card>
-                    </div>
-                  ))}
-                </div>
-              </div>
-              <Button
-                variant="outline"
-                size="icon"
-                className="absolute top-1/2 left-0 transform -translate-y-1/2"
-                onClick={scrollPrev}
-                disabled={!prevBtnEnabled}
-              >
-                <ChevronLeft className="h-4 w-4" />
-                <span className="sr-only">Previous project</span>
-              </Button>
-              <Button
-                variant="outline"
-                size="icon"
-                className="absolute top-1/2 right-0 transform -translate-y-1/2"
-                onClick={scrollNext}
-                disabled={!nextBtnEnabled}
-              >
-                <ChevronRight className="h-4 w-4" />
-                <span className="sr-only">Next project</span>
-              </Button>
-            </div>
+            <h2 className="text-2xl font-semibold mb-6">
+              What I'm working on right now:
+            </h2>
+            <ul className="list-disc pl-5 space-y-1">
+              {" "}
+              {/* Added space-y-2 for vertical spacing between items */}
+              <li>an AI voice assistant to join and help in meetings</li>
+              <li>
+                <a
+                  href="https://www.skander.xyz"
+                  className="text-blue-500 transition-colors duration-400 ease-in-out hover:text-yellow-500 hover:underline"
+                >
+                  this
+                </a>{" "}
+                website
+              </li>
+              <li>
+                solving Leetcode problems, currently solved <LeetCodeStats /> problem.
+              </li>{" "}
+            </ul>
+          </section>
+
+          <section id="past-work">
+            <h2 className="text-2xl font-semibold mb-6">What I've done:</h2>
+            <ul className="list-disc pl-5 space-y-1">
+              <li>
+                Served as a Team Leader for my university's{" "}
+                <a
+                  className="link"
+                  href="https://www.facebook.com/RadioEnetcom/"
+                >
+                  radio club
+                </a>
+                , where we worked on events and digital content
+              </li>
+              <li>Took part in the OST competition where I</li>
+              <li></li>
+            </ul>
           </section>
 
           <section id="contact">
             <h2 className="text-2xl font-semibold mb-4">Get in Touch</h2>
-            <p className="text-muted-foreground mb-4">
-              I&apos;m always open to new opportunities and collaborations. Feel
-              free to reach out!
+            <p className="text-primaryforeground mb-4">
+              I&apos;m always open to new opportunities and collaborations so
+              feel free to reach out to me via email here.
             </p>
-            <Button asChild>
-              <Link href="mailto:skander.karoui@gmail.com">
-                Contact Me <Mail className="ml-2 h-4 w-4" />
-              </Link>
-            </Button>
+            <div className="flex justify-center space-x-4 px-4 my-4 mb-6">
+              <Button asChild>
+                <Link href="mailto:skander.karoui@gmail.com">
+                  contact me <Mail className="ml-2 h-4 w-4" />
+                </Link>
+              </Button>
+            </div>
+            <p className="text-primary foreground mb-4">
+              If you're interested in checking my other socials they're here
+            </p>
+            <div className="flex justify-center space-x-4 px-4 my-4">
+              <Button variant="ghost" size="icon" asChild>
+                <Link
+                  href="https://www.facebook.com/skander.karoui/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Facebook className="h-5 w-5" />
+                  <span className="sr-only">Facebook</span>
+                </Link>
+              </Button>
+              <Button variant="ghost" size="icon" asChild>
+                <Link
+                  href="https://www.linkedin.com/in/skander-karoui"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Linkedin className="h-5 w-5" />
+                  <span className="sr-only">LinkedIn</span>
+                </Link>
+              </Button>
+              <Button variant="ghost" size="icon" asChild>
+                <Link
+                  href="https://www.instagram.com/in/skander_karoui"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Instagram className="h-5 w-5" />
+                  <span className="sr-only">LinkedIn</span>
+                </Link>
+              </Button>
+            </div>
           </section>
         </main>
 
         <footer className="mt-16 text-center text-muted-foreground">
-          <p>© 2024 Skander Karoui. No rights reserved.</p>
+          <p>© 2024 Skander Karoui. No rights reserved 😝.</p>
         </footer>
       </div>
     </div>
