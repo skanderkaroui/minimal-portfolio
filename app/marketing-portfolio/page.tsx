5726e736e96c04ad54138c10d910a9036cbb21c9
skanderkaroui <skander.karoui@gmail.com>
2025-01-23 13:24:48 +0100

    lint fixed

--- app/marketing-portfolio/page.tsx
+++ app/marketing-portfolio/page.tsx
@@ -3,7 +3,6 @@
 import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
-import Image from "next/image";
 
 export default function MarketingPortfolio() {
   return (
